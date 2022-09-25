47d526d0917b32ea2bb826ce3c6e4eaa1bdcc647
Zachary Boelter <zboelter@gmail.com>
2022-09-24 18:25:11 -0700

    game finally works. move prompt into game func. while loop for score. winCondition for end game

--- rps_app.js
+++ rps_app.js
@@ -1,9 +1,25 @@
 
 
-
+// initialize scorea
 let playerScore = 0
 let computerScore = 0
 
+//main funciton for game
+function game(){
+    while(playerScore <= 2 && computerScore <= 2){
+        //prompt user
+        let playerSelection = prompt('Weapon of choice?')
+        let computerSelection = getComputerChoice()
+        playerSelection = playerSelection.toLowerCase()
+        //pop up for rounds
+        alert(playRound(playerSelection, computerSelection))
+        console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
+    }
+    //when condition met, alert winner
+    alert(win_condition())
+}
+
+//random choice for comp
 function getComputerChoice(){
     randomChoice = Math.floor(Math.random() * 3) + 1
     computerChoice = ''
@@ -18,7 +34,7 @@ function getComputerChoice(){
     return computerChoice
 }
 
-
+//conditional logic for gameplay
 function playRound(playerSelection, computerSelection) {
     let message = ''
     //if tie
@@ -28,39 +44,39 @@ function playRound(playerSelection, computerSelection) {
     //if player chose rock
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
         message = 'Rock smashed scissors, Player wins!'
-        playerScore++
+        playerScore += 1
     } else if( playerSelection == 'rock' && computerSelection == 'paper'){
         message = 'Paper wrapped rock, Computer wins this round!'
-        computerScore++
+        computerScore += 1
     }
     //if player chose paper
     if (playerSelection == 'paper' && computerSelection == 'scissors') {
         message = 'Scissors cuts paper, Computer wins.'
-        computerScore++
+        computerScore += 1
     } else if (playerSelection == 'paper' && computerSelection == 'rock'){
         message = 'Paper beats rock, Player wins'
-        playerScore++
+        playerScore += 1
     }
     //if player chose scissors
     if (playerSelection == 'scissors' && computerSelection == 'rock') {
         message = 'Scissors loses to rock, Computer wins.'
-        computerScore++
+        computerScore += 1
     } else if (playerSelection == 'scissors' && computerSelection == 'paper'){
         message = 'Paper loses to scissors, Player wins'
-        playerScore++
+        playerScore += 1
     }
-    return message
-    
-  }
-   
-  const playerSelection = prompt('Weapon of choice?').toLowerCase()
-  const computerSelection = getComputerChoice()
-  console.log(playRound(playerSelection, computerSelection))
-
-  
-
-function game(){
-    playRound(playerSelection, computerSelection)
-    console.log(playerScore, computerscore)
+    return message 
 }
+
+//function to end game
+function win_condition() {
+    if (playerScore < computerScore) {
+      return 'Computer Wins!';
+    } else {
+        return 'Player Wins'
+    }
+    
+}
+
+//play game
 console.log(game())
