6af4a412217d7210af80cf888b9bca19e7782417
Zachary Boelter <zboelter@gmail.com>
2022-09-25 16:44:24 -0700

    remove unnecessary comments

--- rps_app.js
+++ rps_app.js
@@ -1,25 +1,24 @@
 
 
-// initialize scorea
+
 let playerScore = 0
 let computerScore = 0
 
-//main funciton for game
+
 function game(){
     while(playerScore <= 2 && computerScore <= 2){
-        //prompt user
+
         let playerSelection = prompt('Weapon of choice?')
         let computerSelection = getComputerChoice()
         playerSelection = playerSelection.toLowerCase()
-        //pop up for rounds
+
         alert(playRound(playerSelection, computerSelection))
         console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
     }
-    //when condition met, alert winner
     alert(win_condition())
 }
 
-//random choice for comp
+
 function getComputerChoice(){
     randomChoice = Math.floor(Math.random() * 3) + 1
     computerChoice = ''
@@ -34,7 +33,7 @@ function getComputerChoice(){
     return computerChoice
 }
 
-//conditional logic for gameplay
+
 function playRound(playerSelection, computerSelection) {
     let message = ''
     //if tie
@@ -68,7 +67,7 @@ function playRound(playerSelection, computerSelection) {
     return message 
 }
 
-//function to end game
+
 function winCondition() {
     if (playerScore < computerScore) {
       return 'Computer Wins!';
@@ -78,5 +77,5 @@ function winCondition() {
     
 }
 
-//play game
+
 console.log(game())
