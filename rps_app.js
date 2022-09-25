1c11d2fe6d950fd879854a96f350eff038f373bd
Zachary Boelter <zboelter@gmail.com>
2022-09-24 17:02:58 -0700

    function to getComputerChoice via random num set to game strings

--- rps_app.js
+++ rps_app.js
@@ -1,1 +1,16 @@
-console.log('hello there slut')
+
+
+function getComputerChoice(){
+    randomChoice = Math.floor(Math.random() * 3) + 1
+    computerChoice = ''
+
+    if (randomChoice === 1){
+        computerChoice = 'rock'
+    } else if (randomChoice === 2){
+        computerChoice = 'paper'
+    } else {
+        computerChoice = 'scissors'
+    }
+    console.log(computerChoice)
+}
+getComputerChoice()
