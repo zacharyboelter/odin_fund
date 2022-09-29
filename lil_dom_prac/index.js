787b3a9ba12d81e60de1142828ed4e6819590a2a
Zachary Boelter <zboelter@gmail.com>
2022-09-29 16:23:21 -0700

    back to basics

--- lil_dom_prac/index.js
+++ lil_dom_prac/index.js
@@ -13,3 +13,20 @@ blue.textContent = 'I am a blue h3'
 blue.style.color = 'blue'
 container.appendChild(blue)
 
+const newDiv = document.createElement('div')
+newDiv.style.cssText = 'border: 1px solid black; background: pink'
+
+const h1 = document.createElement('h1')
+h1.textContent = "I'm in a div"
+const p = document.createElement('p')
+p.textContent = 'Me too!'
+
+newDiv.appendChild(h1)
+newDiv.appendChild(p)
+
+
+
+
+
+
+container.appendChild(newDiv)
