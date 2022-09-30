be4fa6438eb4172d87b4e922d1fab6543efab91c
Zachary Boelter <zboelter@gmail.com>
2022-09-30 16:18:43 -0700

    copied over from non repo tabs

--- lil_dom_prac/index.js
+++ lil_dom_prac/index.js
@@ -1,30 +1,30 @@
 
 
-const para = document.createElement('p')
+// const para = document.createElement('p')
 
-para.classList.add('paragraph-red')
-para.textContent = 'Hey, I am red!'
-para.style.color = 'red'
+// para.classList.add('paragraph-red')
+// para.textContent = 'Hey, I am red!'
+// para.style.color = 'red'
 
-container.appendChild(para)
+// container.appendChild(para)
 
-const blue = document.createElement('h3')
-blue.textContent = 'I am a blue h3'
-blue.style.color = 'blue'
-container.appendChild(blue)
+// const blue = document.createElement('h3')
+// blue.textContent = 'I am a blue h3'
+// blue.style.color = 'blue'
+// container.appendChild(blue)
 
-const newDiv = document.createElement('div')
-newDiv.style.cssText = 'border: 1px solid black; background: pink'
+// const newDiv = document.createElement('div')
+// newDiv.style.cssText = 'border: 1px solid black; background: pink'
 
-const h1 = document.createElement('h1')
-h1.textContent = "I'm in a div"
-const p = document.createElement('p')
-p.textContent = 'Me too!'
+// const h1 = document.createElement('h1')
+// h1.textContent = "I'm in a div"
+// const p = document.createElement('p')
+// p.textContent = 'Me too!'
 
-newDiv.appendChild(h1)
-newDiv.appendChild(p)
+// newDiv.appendChild(h1)
+// newDiv.appendChild(p)
 
-container.appendChild(newDiv)
+// container.appendChild(newDiv)
 
 // const btn = document.querySelector('#btn');
 // btn.onclick = () => alert("Hello World");
@@ -46,10 +46,31 @@ container.appendChild(newDiv)
 //     e.target.style.background = 'blue';
 // });
 
-const buttons = document.querySelectorAll('button')
+// const buttons = document.querySelectorAll('button')
 
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        alert(button.id)
-    })
-})
+// buttons.forEach((button) => {
+//     button.addEventListener('click', () => {
+//         alert(button.id)
+//     })
+// })
+
+
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return
+    e.target.classList.remove('playing')
+}
+
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+    if(!audio) return
+   
+    key.classList.add('playing')
+    audio.currentTime = 0
+    audio.play()
+}
+
+const keys = Array.from(document.querySelectorAll('.key'))
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound)
