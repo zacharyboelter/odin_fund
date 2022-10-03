0481d548d82bbad7fb3321299aa3986a572ad9a3
Zachary Boelter <zboelter@gmail.com>
2022-10-03 13:51:55 -0700

    game is kinda working. Need to figure out update score count in dom and endgame

--- rsp/rps_app.js
+++ rsp/rps_app.js
@@ -5,88 +5,88 @@ const message = document.getElementById('message')
 const possibleChoices = document.querySelectorAll('button')
 
 
-let playerScore = 0
-let computerScore = 0
+
 let playerChoice
 let computerChoice
-
-playerScoreSpan.textContent = playerScore
-computerScoreSpan.textContent = computerScore
-
-
-
-
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    message.innerHTML = `${userChoice}`
-    getComputerChoice()
-    playRound()
-  }))
-
-
-
-
 // function game(){
 //     while(playerScore < 5 && computerScore < 5){
-
-        
-
 //         playRound(playerSelection, computerSelection)
 //         console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
 //     }
 //     winCondition()
 // }
 
+let playerScore = 0
+let computerScore = 0
 
-function getComputerChoice(){
+
+
+
+function getComputerChoice() {
     randomChoice = Math.floor(Math.random() * 3) + 1
-    computerChoice = ''
+    computerSelection = ''
 
-    if (randomChoice === 1){
-        computerChoice = 'rock'
-    } else if (randomChoice === 2){
-        computerChoice = 'paper'
+    if (randomChoice === 1) {
+        computerSelection = 'rock'
+    } else if (randomChoice === 2) {
+        computerSelection = 'paper'
     } else {
-        computerChoice = 'scissors'
+        computerSelection = 'scissors'
     }
-    return computerChoice
+    return computerSelection
 }
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    playerSelection = e.target.id
+    message.innerHTML = `${playerSelection}`
+    getComputerChoice()
+    playRound(playerSelection, computerSelection)
+    console.log(computerSelection)
+    console.log(playerSelection)
+    console.log(playerScore)
+    console.log(computerScore)
+}))
 
 
 function playRound(playerSelection, computerSelection) {
+   
+    while (playerScore < 5 && computerScore < 5) {
 
-    
-    
-    message.textContent = ''
-    //if tie
-    if (playerSelection == computerSelection){
-        message.textContent = 'Its a tie!'
-    } 
-    //if player chose rock
-    if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        message.textContent = 'Rock smashed scissors, Player wins!'
-        playerScore += 1
-    } else if( playerSelection == 'rock' && computerSelection == 'paper'){
-        message.textContent = 'Paper wrapped rock, Computer wins this round!'
-        computerScore += 1
+        message.textContent = ''
+        //if tie
+        if (playerSelection == computerSelection) {
+            message.textContent = 'Its a tie!'
+        }
+        //if player chose rock
+        if (playerSelection == 'rock' && computerSelection == 'scissors') {
+            message.textContent = 'Rock smashed scissors, Player wins!'
+            playerScore += 1
+        } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+            message.textContent = 'Paper wrapped rock, Computer wins this round!'
+            computerScore += 1
+        }
+        //if player chose paper
+        if (playerSelection == 'paper' && computerSelection == 'scissors') {
+            message.textContent = 'Scissors cuts paper, Computer wins.'
+            computerScore += 1
+        } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+            message.textContent = 'Paper beats rock, Player wins'
+            playerScore += 1
+        }
+        //if player chose scissors
+        if (playerSelection == 'scissors' && computerSelection == 'rock') {
+            message.textContent = 'Scissors loses to rock, Computer wins.'
+            computerScore += 1
+        } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+            message.textContent = 'Paper loses to scissors, Player wins'
+            playerScore += 1
+        }
+        winCondition()
+        return message
+        playerScoreSpan.textContent = playerScore
+        computerScoreSpan.textContent = computerScore
     }
-    //if player chose paper
-    if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        message.textContent = 'Scissors cuts paper, Computer wins.'
-        computerScore += 1
-    } else if (playerSelection == 'paper' && computerSelection == 'rock'){
-        message.textContent = 'Paper beats rock, Player wins'
-        playerScore += 1
-    }
-    //if player chose scissors
-    if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        message.textContent = 'Scissors loses to rock, Computer wins.'
-        computerScore += 1
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper'){
-        message.textContent = 'Paper loses to scissors, Player wins'
-        playerScore += 1
-    }
-    return message.textContent 
+
 }
 
 
@@ -96,7 +96,7 @@ function winCondition() {
     } else {
         console.log('Player Wins')
     }
-    
+
 }
 
 // function getPlayerChoice(e){
