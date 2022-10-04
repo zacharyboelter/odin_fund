bff67e0dc720b34492cba5f60433ae9cb465365c
Zachary Boelter <zboelter@gmail.com>
2022-10-04 14:57:57 -0700

    Multiply numeric property values by 2

--- objects/app.js
+++ objects/app.js
@@ -30,6 +30,24 @@ for (let key in salaries){
     sum += salaries[key]
 }
 
-console.log(sum)
-console.log(user)
-console.log(isEmpty(user))
+
+//Multiply numeric property values by 2
+
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+function multiplyNumeric(obj){
+    for (let key in obj){
+        if (typeof obj[key] == 'number'){
+            obj[key] *= 2
+        }
+    }
+    return obj
+}
+
+
+console.log(multiplyNumeric(menu))
