cb40797b43ac4eca9ef2181d501bdd078778ade3
Zachary Boelter <zboelter@gmail.com>
2022-10-04 14:46:41 -0700

    isEmpty function that returns false if object has key:value. True otherwise

--- objects/app.js
+++ objects/app.js
@@ -1,3 +1,19 @@
 
 
-console.log('hello world')
+let user = {}
+
+user.name = 'john'
+user.surName = 'smith'
+user.name = 'Pete'
+delete user.name
+
+
+function isEmpty(obj){
+    for (let key in obj){
+        return false
+    } 
+    return true
+}
+
+console.log(user)
+console.log(isEmpty(user))
