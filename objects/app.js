5bbac199bfd12d7ae89ef9770b3bee215142f738
Zachary Boelter <zboelter@gmail.com>
2022-10-04 14:49:59 -0700

    sum object properties

--- objects/app.js
+++ objects/app.js
@@ -7,6 +7,7 @@ user.surName = 'smith'
 user.name = 'Pete'
 delete user.name
 
+// Check for emptiness
 
 function isEmpty(obj){
     for (let key in obj){
@@ -15,5 +16,20 @@ function isEmpty(obj){
     return true
 }
 
+
+// Sum object properties
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
+
+let sum = 0
+for (let key in salaries){
+    sum += salaries[key]
+}
+
+console.log(sum)
 console.log(user)
 console.log(isEmpty(user))
